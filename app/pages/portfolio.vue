17b8384383304d224e9b907a04aae2d82577fe56
jadedQuail <jcantie3@gmail.com>
2026-08-21 12:15:33 -0400

    #2 added portfolio page stub

---
+++ app/pages/portfolio.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({ layout: 'page' })
+
+useSeoMeta({ title: 'Portfolio' })
+</script>
+
+<template>
+  <h1 class="text-coral font-display mb-10 text-4xl">Portfolio</h1>
+
+  <p class="text-mist">TODO-JOSH: portfolio items go here.</p>
+</template>
